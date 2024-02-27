b06c4ed02ac7a0cfda3efda58bafeba8a8608fe9
imon <faruk.ahamad@orangetoolz.com>
2024-02-27 23:36:02 +0600

    configured C3PO conenction pooling for db connection

--- src/main/java/com/imon/jobapp/company/CompanyRepository.java
+++ src/main/java/com/imon/jobapp/company/CompanyRepository.java
@@ -1,7 +1,9 @@
 package com.imon.jobapp.company;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CompanyRepository extends JpaRepository<Company, Long> {
 
 }
--- src/main/java/com/imon/jobapp/config/DataConfig.java
+++ src/main/java/com/imon/jobapp/config/DataConfig.java
@@ -8,6 +8,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+import com.mchange.v2.c3p0.jboss.C3P0PooledDataSource;
+import com.zaxxer.hikari.HikariDataSource;
+
 @Configuration
 public class DataConfig {
 
@@ -20,17 +24,45 @@ public class DataConfig {
     @Value("${spring.datasource.password}")
     private String password;
 
+    // This manual dataSource Bean doesn't provide connection pool so we should
+    // not use in the production
+    // @Bean
+    // public DataSource dataSource() {
+    // DriverManagerDataSource dataSource = new DriverManagerDataSource();
+    // dataSource.setUrl(url);
+    // dataSource.setUsername(username);
+    // dataSource.setPassword(password);
+    // return dataSource;
+    // }
+
+    // This is manually setting Hikaripool connection pooling
+    // @Bean
+    // public DataSource dataSource() {
+    // HikariDataSource dataSource = new HikariDataSource();
+    // dataSource.setJdbcUrl(url);
+    // dataSource.setUsername(username);
+    // dataSource.setPassword(password);
+    // return dataSource;
+    // }
+
+    // setting C3PO connection pooling mechanism instead of default Hikaripool
+    // connection pooling
     @Bean
-    public DataSource dataSource() {
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setUrl(url);
-        dataSource.setUsername(username);
+    DataSource dataSource() {
+        ComboPooledDataSource dataSource = new ComboPooledDataSource();
+        dataSource.setJdbcUrl(url);
+        dataSource.setDataSourceName(username);
         dataSource.setPassword(password);
+
+        dataSource.setMinPoolSize(5);
+        dataSource.setMaxPoolSize(20);
+        dataSource.setMaxIdleTime(300);
+        dataSource.setMaxStatements(50);
         return dataSource;
     }
 
     @Bean
-    public JdbcTemplate jdbcTemplate() {
+    JdbcTemplate jdbcTemplate() {
         return new JdbcTemplate(dataSource());
     }
 
