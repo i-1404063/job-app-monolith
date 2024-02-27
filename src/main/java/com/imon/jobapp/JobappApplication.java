3102a5cd530830aeb5e7736ead56cab63f554577
imon <faruk.ahamad@orangetoolz.com>
2024-02-27 23:06:56 +0600

    manual dataSource configuration

--- src/main/java/com/imon/jobapp/JobappApplication.java
+++ src/main/java/com/imon/jobapp/JobappApplication.java
@@ -1,13 +1,42 @@
 package com.imon.jobapp;
 
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.JdbcTemplateAutoConfiguration;
+import org.springframework.jdbc.core.JdbcTemplate;
 
-@SpringBootApplication
-public class JobappApplication {
+@SpringBootApplication(exclude = {
+		JdbcTemplateAutoConfiguration.class,
+		DataSourceAutoConfiguration.class
+})
+public class JobappApplication implements CommandLineRunner {
+
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
 
 	public static void main(String[] args) {
 		SpringApplication.run(JobappApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("application started");
+
+		List<Map<String, Object>> queryForList = jdbcTemplate.queryForList("Select * from company");
+
+		queryForList.forEach(item -> {
+			System.out.println("id " + item.get("id"));
+			System.out.println("description " + item.get("description"));
+			System.out.println("name " + item.get("name"));
+
+			System.out.println("--------------------------");
+		});
 	}
 
 }
